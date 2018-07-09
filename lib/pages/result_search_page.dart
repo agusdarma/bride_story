23ec8c538e131181da7177eb338d4becc9a1e7e2
agusdarma <agus.darma@emobile.com.sg>
2018-07-10 00:40:19 +0700

    update ui

--- lib/pages/result_search_page.dart
+++ lib/pages/result_search_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../data/filter_param.dart';
@@ -11,57 +13,108 @@ class ResultSearchPage extends StatefulWidget {
 }
 
 class _ResultSearchPageState extends State<ResultSearchPage> {
+  SharedPreferences sharedPreferences;
+  List<TextSpan> listSubtitle;
+  Future<List<TextSpan>> _getFilterParam() async {
+    try {
+      sharedPreferences = await SharedPreferences.getInstance();
+      String pref = sharedPreferences.getString(keyFilterParam);
+      const JsonDecoder decoder = const JsonDecoder();
+      Map filterParamMap = decoder.convert(pref);
+      var filterParamNew = new FilterParam.fromJson(filterParamMap);
+      listSubtitle = new List<TextSpan>();
+      listSubtitle.add(new TextSpan(text: "Showing result for "));
+      listSubtitle.add(new TextSpan(text: filterParamNew.categoryName));
+      listSubtitle.add(new TextSpan(text: " "));
+      listSubtitle.add(new TextSpan(text: "in "));
+      listSubtitle.add(new TextSpan(text: filterParamNew.cityName));
+      listSubtitle.add(new TextSpan(text: " "));
+      listSubtitle.add(new TextSpan(text: filterParamNew.countryName));
+      listSubtitle.add(new TextSpan(text: " "));
+      // print(listSubtitle);
+      return listSubtitle;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // setState(() {
+    //   _getFilterParam();
+    // });
+    _getFilterParam().then((result) {
+      // If we need to rebuild the widget with the resulting data,
+      // make sure to use `setState`
+      setState(() {
+        listSubtitle = result;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // _getFilterParam();
+    print(listSubtitle);
     Widget subtitle = new Container(
-      padding: new EdgeInsets.all(12.0),
-      color: new Color(0X33000000),
-      child:
-          new Text('Showing result for All Categories in Jakarta, Indonesia'),
-    );
+        padding: new EdgeInsets.all(12.0),
+        color: new Color(0X33000000),
+        child: new RichText(
+          text: new TextSpan(
+              children: listSubtitle,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14.0,
+              )),
+        ));
 
     Widget listImagesContent = new Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         new Container(
-            // padding: EdgeInsets.only(right: 5.0),
+            margin: EdgeInsets.only(top: 5.0),
             child: new Image(
-          width: 100.0,
-          height: 100.0,
-          image: new AssetImage('assets/images/1.jpg'),
-        )),
+              fit: BoxFit.fitHeight,
+              width: 90.0,
+              height: 90.0,
+              image: new AssetImage('assets/images/1.jpg'),
+            )),
         new Container(
-            // padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.only(top: 5.0),
             child: new Image(
-          width: 100.0,
-          height: 100.0,
-          image: new AssetImage('assets/images/1.jpg'),
-        )),
+              fit: BoxFit.fitHeight,
+              width: 90.0,
+              height: 90.0,
+              image: new AssetImage('assets/images/1.jpg'),
+            )),
         new Container(
-            // padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.only(top: 5.0),
             child: new Image(
-          width: 100.0,
-          height: 100.0,
-          image: new AssetImage('assets/images/1.jpg'),
-        )),
+              fit: BoxFit.fitHeight,
+              width: 90.0,
+              height: 90.0,
+              image: new AssetImage('assets/images/1.jpg'),
+            )),
         new Container(
-            // padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.only(top: 5.0),
             child: new Image(
-          width: 100.0,
-          height: 100.0,
-          image: new AssetImage('assets/images/1.jpg'),
-        )),
+              fit: BoxFit.fitHeight,
+              width: 90.0,
+              height: 90.0,
+              image: new AssetImage('assets/images/1.jpg'),
+            )),
       ],
     );
 
     Widget content = new Container(
-        padding: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(5.0),
         child: new Row(
           children: <Widget>[
             new Container(
-                margin: EdgeInsets.only(right: 10.0),
-                width: 70.0,
-                height: 70.0,
+                margin: EdgeInsets.only(right: 5.0),
+                width: 60.0,
+                height: 60.0,
                 decoration: new BoxDecoration(
                     shape: BoxShape.circle,
                     image: new DecorationImage(
@@ -73,18 +126,18 @@ class _ResultSearchPageState extends State<ResultSearchPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   new Container(
-                    padding: const EdgeInsets.only(bottom: 8.0),
+                    padding: const EdgeInsets.only(bottom: 3.0),
                     child: new Text("Jova Musique",
                         style: TextStyle(
-                          fontSize: 14.0,
+                          fontSize: 11.0,
                           fontWeight: FontWeight.bold,
                         )),
                   ),
                   new Container(
-                    padding: const EdgeInsets.only(bottom: 8.0),
+                    padding: const EdgeInsets.only(bottom: 3.0),
                     child: new Text("Entertainment Music,Jakarta",
                         style: TextStyle(
-                          fontSize: 14.0,
+                          fontSize: 11.0,
                         )),
                   ),
                   new Row(
@@ -95,7 +148,7 @@ class _ResultSearchPageState extends State<ResultSearchPage> {
                       new Icon(Icons.star),
                       new Text("32 Reviews >",
                           style: TextStyle(
-                            fontSize: 14.0,
+                            fontSize: 11.0,
                           ))
                     ],
                   ),
@@ -106,9 +159,7 @@ class _ResultSearchPageState extends State<ResultSearchPage> {
         ));
 
     Widget rowData = new Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[content, listImagesContent, new Divider(height: 2.0)],
+      children: <Widget>[content, listImagesContent, new Divider(height: 10.0)],
     );
 
     return SafeArea(
